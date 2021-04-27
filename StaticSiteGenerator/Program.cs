476c0c78ad1a91c46524656b0cfe822d1d13e2d3
TonySauceda <sauceda92@hotmail.com>
2021-04-26 20:48:17 -0600

    Agregar ruleset de la guia de estilos

--- StaticSiteGenerator/Program.cs
+++ StaticSiteGenerator/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
-
-namespace StaticSiteGenerator
+﻿namespace StaticSiteGenerator
 {
-    class Program
+    using System;
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
