9b55f6266958864de053558507ca6280789393a8
TonySauceda <sauceda92@hotmail.com>
2021-04-26 23:59:51 -0600

    Agregar CommandLineUtils

--- StaticSiteGenerator/Program.cs
+++ StaticSiteGenerator/Program.cs
@@ -1,12 +1,23 @@
 ﻿namespace StaticSiteGenerator
 {
     using System;
+    using McMaster.Extensions.CommandLineUtils;
 
+    [Command("Static Site Generator")]
+    [VersionOptionFromMember("--version", MemberName = nameof(Version))]
     public class Program
     {
+        public string Version { get; } = "0.0.0";
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandLineApplication.Execute<Program>(args);
+        }
+
+        public int OnExecute(CommandLineApplication app)
+        {
+            app.ShowHelp();
+            return 0;
         }
     }
 }
