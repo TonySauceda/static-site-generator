e335977cef6f654a96eb7c9fae57c868a14975de
TonySauceda <sauceda92@hotmail.com>
2021-04-29 19:21:35 -0600

    Agregar CopyFiles y sus pruebas

--- StaticSiteGenerator.Test/SiteBuilderTest.cs
+++ StaticSiteGenerator.Test/SiteBuilderTest.cs
@@ -3,6 +3,7 @@
     using Moq;
     using Shouldly;
     using StaticSiteGenerator.Builder;
+    using System.Collections.Generic;
     using System.IO.Abstractions;
     using System.IO.Abstractions.TestingHelpers;
     using System.Linq;
@@ -53,19 +54,13 @@
             AssertDirectoryIsEmpty(output);
         }
 
-        private void AssertDirectoryIsEmpty(string output)
-        {
-            fakeFileSystem.Directory.Exists(output).ShouldBeTrue();
-            fakeFileSystem.Directory.EnumerateFiles(output).Any().ShouldBeFalse();
-            fakeFileSystem.Directory.EnumerateDirectories(output).Any().ShouldBeFalse();
-        }
-
         [Fact]
-        public void TestBuildCallsClean()
+        public void TestBuildCallsCleanAndCopy()
         {
             // Setup
             var mockSiteBuilder = new Mock<CLISiteBuilder>(MockBehavior.Strict, this.fakeFileSystem);
             mockSiteBuilder.Setup(x => x.CleanFolder(output));//Para que no se ejecute el contenido de la funcion, ya que no entra en el scope de la prueba
+            mockSiteBuilder.Setup(x => x.CopyFiles(input, output));
             var siteBuilder = mockSiteBuilder.Object;
 
             // Act
@@ -73,6 +68,38 @@
 
             // Assert
             mockSiteBuilder.Verify(x => x.CleanFolder(output));
+            mockSiteBuilder.Verify(x => x.CopyFiles(input, output));
+        }
+
+        [Fact]
+        public void TestCopyFiles()
+        {
+            // Setup
+            var stylesFile = fakeFileSystem.Path.Combine(input, "style.css");
+            var someOtherFile = fakeFileSystem.Path.Combine(input, "subFolder", "sybStyle.css");
+
+            var content = new Dictionary<string, MockFileData>
+            {
+                { stylesFile, new MockFileData("body { color: #fff; }") },
+                { someOtherFile, new MockFileData("body { color: #aaa; }") }
+            };
+
+            var fakeFIleSystem = new MockFileSystem(content);
+            fakeFIleSystem.Directory.CreateDirectory(output);
+            var siteBuilder = new CLISiteBuilder(fakeFIleSystem);
+
+            // Act
+            siteBuilder.CopyFiles(input, output);
+
+            // Assert
+            fakeFIleSystem.Directory.EnumerateFiles(output, "*.*", System.IO.SearchOption.AllDirectories).Count().ShouldBe(2);
+        }
+
+        private void AssertDirectoryIsEmpty(string output)
+        {
+            fakeFileSystem.Directory.Exists(output).ShouldBeTrue();
+            fakeFileSystem.Directory.EnumerateFiles(output).Any().ShouldBeFalse();
+            fakeFileSystem.Directory.EnumerateDirectories(output).Any().ShouldBeFalse();
         }
     }
 }
--- StaticSiteGenerator/Builder/CLISiteBuilder.cs
+++ StaticSiteGenerator/Builder/CLISiteBuilder.cs
@@ -1,5 +1,6 @@
 ﻿namespace StaticSiteGenerator.Builder
 {
+    using System;
     using System.IO.Abstractions;
 
     public class CLISiteBuilder : ISiteBuilder
@@ -21,9 +22,32 @@
             this.fileSystem.Directory.CreateDirectory(folder);
         }
 
+        public virtual void CopyFiles(string input,string output)
+        {
+            var source = this.fileSystem.DirectoryInfo.FromDirectoryName(input);
+            var target = this.fileSystem.DirectoryInfo.FromDirectoryName(output);
+
+            this.CopyFiles(source, target);
+        }
+
         public void Build(string inputPath, string outputPath)
         {
             this.CleanFolder(outputPath);
+            this.CopyFiles(inputPath, outputPath);
+        }
+
+        private void CopyFiles(IDirectoryInfo source, IDirectoryInfo target)
+        {
+            foreach (var file in source.GetFiles())
+            {
+                file.CopyTo(this.fileSystem.Path.Combine(target.FullName, file.Name));
+            }
+
+            foreach (var subDir in source.GetDirectories())
+            {
+                var nextTargetSubDir = target.CreateSubdirectory(subDir.Name);
+                this.CopyFiles(subDir, nextTargetSubDir);
+            }
         }
     }
 }
